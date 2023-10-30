bf829d970bb97504cdea79811df3b97e1fc7f2d8
Kyle Lund <unit0113@gmail.com>
2023-10-30 18:07:15 -0500

    New starting point for sprint 1

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "eb67804bd55231ed"; }
+/******/ 		__webpack_require__.h = function() { return "0829b69f309e31a2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/lang/Desktop/Projects/EcoFriend/src/app/page.tsx
+// File: C:\Users\unit0\Desktop\Python\EcoFriend\src\app\page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
