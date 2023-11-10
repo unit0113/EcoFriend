4fd1d3b389df35a82bc7080544808a26d23cf055
christian-carey <113315529+christian-carey@users.noreply.github.com>
2023-11-09 20:21:02 -0500

    Updated login page unit tests, removed about page unit tests

--- test/app/login/page.test.tsx
+++ test/app/login/page.test.tsx
@@ -1,47 +1,28 @@
-// import { render, screen, fireEvent } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
-// import Page from '@/app/login/page';
-// import { PageWithSessionProvider } from '@/app/login/page';
-// import { act } from 'react-dom/test-utils'; // import act for async rendering
+import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Page from '@/app/login/page';
 
+jest.mock('next/navigation', () => ({
+  useRouter: () => ({ replace: jest.fn() }),
+}));
 
-// jest.mock('next/navigation', () => ({
-//   useRouter: () => ({ replace: jest.fn() }),
-// }));
-
-// // describe('Page Component', () => {
-// //   test('renders without crashing', () => {
-// //     render(<Page />);
-// //     expect(screen.getByText('Sign in to your account')).toBeInTheDocument();
-// //   });
-
-// describe('Page Component', () => {
-//   test('renders without crashing', () => {
-//     render(<PageWithSessionProvider />);
-//     // Add more specific assertions if needed
-//     expect(screen.getByText('Sign in to your account')).toBeInTheDocument();
-//   });
-
-//   test('renders without crashing', async () => {
-//     await act(async () => {
-//       render(<Page />);
-//     });
-//   });
+describe('Page Component', () => {
+  test('renders without crashing', () => {
+    render(<Page />);
+    expect(screen.getByText('Sign in to your account')).toBeInTheDocument();
+  });
   
+  test('submits the form correctly', async () => {
+    render(<Page />);
 
-//   // test('submits the form correctly', async () => {
-//   //   render(<Page />);
+    // Fill in form fields
+    userEvent.type(screen.getByLabelText(/email address/i), 'test@example.com');
+    userEvent.type(screen.getByLabelText(/password/i), 'password');
 
-//   //   // Fill in form fields
-//   //   userEvent.type(screen.getByLabelText(/email address/i), 'test@example.com');
-//   //   userEvent.type(screen.getByLabelText(/password/i), 'password');
+    // Simulate form submission
+    fireEvent.submit(screen.getByRole('button', { name: /sign in/i }));
 
-//   //   // Simulate form submission
-//   //   fireEvent.submit(screen.getByRole('button', { name: /sign in/i }));
-
-//   //   // Expect that the handleSubmit function is called
-//   //   // You may need to adjust this based on the actual behavior of your component
-//   //   // For example, if the login is successful, it would redirect, so you might want to check for that
-//   //   expect(screen.getByText('Invalid login information')).toBeInTheDocument();
-//   // });
-// });
+    // Expect that the handleSubmit function is called
+    expect(screen.getByText('Invalid login information')).toBeInTheDocument();
+  });
+});
