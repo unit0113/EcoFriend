2145048f0ff994c52f9aca6ce5cb1f25970a29a5
Kyle Lund <unit0113@gmail.com>
2023-11-14 16:42:54 -0600

    Schema and api's for teams created

---
+++ src/app/api/createTeam/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { connectMongoDB } from '@/lib/mongodb'
+import Team from '@/models/team'
+import User from '@/models/user'
+import bcrypt from 'bcryptjs'
+
+export async function POST(req: NextRequest) {
+  try {
+    await connectMongoDB()
+    const { name, password, userEmail } = await req.json()
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const user = await User.findOne({ userEmail })
+
+    const team = await Team.create({
+      name: name,
+      password: hashedPassword,
+      members: [user._id],
+    })
+    await user.updateOne({ team_id: team._id })
+
+    return NextResponse.json({ message: 'New team created' }, { status: 201 })
+  } catch (error) {
+    return NextResponse.json(
+      { message: 'An error occured while creating a new team.' },
+      { status: 500 },
+    )
+  }
+}
