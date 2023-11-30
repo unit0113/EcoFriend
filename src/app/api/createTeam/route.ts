4e49fd0c7c30268fe7ddb4d4e7cb75a09a1c2c16
0xedl <122504006+0xedl@users.noreply.github.com>
2023-11-29 22:50:22 -0500

    create team implement, next implement will be join teams and fix team page.

--- src/app/api/createTeam/route.ts
+++ src/app/api/createTeam/route.ts
@@ -1,39 +1,46 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { connectMongoDB } from '@/lib/mongodb'
+import {NextRequest, NextResponse} from 'next/server'
+import {connectMongoDB} from '@/lib/mongodb'
 import Team from '@/models/team'
 import User from '@/models/user'
 import bcrypt from 'bcryptjs'
 
 export async function POST(req: NextRequest) {
-  try {
-    await connectMongoDB()
-    const { name, password, userEmail } = await req.json()
+    try {
+        await connectMongoDB()
 
-    // Find existing team with provided name
-    const existingTeam = await Team.findOne({ name })
-    if (existingTeam) {
-      return NextResponse.json(
-        { message: 'Team name already exists' },
-        { status: 401 },
-      )
+        const {name, password, userEmail} = await req.json()
+        console.log('Passed req: ', name, password, userEmail);
+
+        const existingTeam = await Team.findOne({name})
+        if (existingTeam) {
+            return NextResponse.json(
+                {message: 'Team name already exists'},
+                {status: 401},
+            )
+        }
+        console.log("Passed existingTeam check");
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+        console.log("Passed hashedPass check");
+
+        const user = await User.findOne({email: userEmail})
+        console.log("Passed findUser check");
+
+        const team = await Team.create({
+            name: name,
+            password: hashedPassword,
+            members: [user._id],
+        })
+        console.log("Passed team creation check", team._id);
+
+        await User.updateOne({_id: user._id}, {$set: {team_id: team._id}});
+        console.log("Passed update user check");
+
+        return NextResponse.json({message: 'New team created'}, {status: 201})
+    } catch (error) {
+        return NextResponse.json(
+            {message: 'An error occurred while creating a new team.'},
+            {status: 500},
+        )
     }
-
-    // Hash password, create team, and attach team to user
-    const hashedPassword = await bcrypt.hash(password, 10)
-    const user = await User.findOne({ userEmail })
-
-    const team = await Team.create({
-      name: name,
-      password: hashedPassword,
-      members: [user._id],
-    })
-    await user.updateOne({ team_id: team._id })
-
-    return NextResponse.json({ message: 'New team created' }, { status: 201 })
-  } catch (error) {
-    return NextResponse.json(
-      { message: 'An error occurred while creating a new team.' },
-      { status: 500 },
-    )
-  }
 }
